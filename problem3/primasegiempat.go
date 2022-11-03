63ba7c599453d947cbf0bd9df9c3d494ef42362c
Anti-Gen <putrategu@gmail.com>
2022-11-03 16:44:52 +0700

    Tugas-Revisi

--- problem3/primasegiempat.go
+++ problem3/primasegiempat.go
@@ -1,14 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func checkNumber(number int) bool {
+	if number < 2 {
+		return false
+	}
+	sqrtNumber := int(math.Sqrt(float64(number)))
+	for i := 2; i <= sqrtNumber; i++ {
+		if number%i == 0 {
+			return false
+		}
+
+	}
+	return true
+
+}
 
 func PrimaSegiEmpat(wide, high, start int) string {
+	var result string
+	var number int = start
+	var sumNumber int
+
+	for i := 1; i <= high; i++ {
+		for j := 1; j <= wide; j++ {
+			number++
+
+			for !checkNumber(number) { //ketika fo false maka akan dilanjutkan ke proses selanjutnya jka masih true akan terus berulang.
+				number++
+			}
+			result = result + fmt.Sprintf("%d", number) + "	"
+			sumNumber = sumNumber + number
+		}
+		result = result + "\n"
+
+	}
+	result += fmt.Sprintf("%d", sumNumber)
+	return result
 
 }
 
 func main() {
 
+	// fmt.Println("=========================")
 	fmt.Println(PrimaSegiEmpat(2, 3, 13))
+	// fmt.Println("=========================")
+	// fmt.Println(checkNumber(15))
 	// /*
 	// 	17	19
 	// 	23	29
@@ -16,9 +56,9 @@ func main() {
 	// 	156
 	// */
 	fmt.Println(PrimaSegiEmpat(5, 2, 1))
-	// /*
-	// 	2	3	5	7	11
-	// 	13	17	19	23	29
-	// 	129
-	// */
+	// // /*
+	// // 	2	3	5	7	11
+	// // 	13	17	19	23	29
+	// // 	129
+	// // */
 }
