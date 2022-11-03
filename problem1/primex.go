1841e288d80c43c42ca426e81eb52704d6b2d292
Anti-Gen <putrategu@gmail.com>
2022-11-03 19:18:57 +0700

    Revisi_Nomor_1

--- problem1/primex.go
+++ problem1/primex.go
@@ -1,16 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func checkNumber(number int) bool {
+	if number < 2 {
+		return false
+	}
+	sqrtNumber := int(math.Sqrt(float64(number)))
+	for i := 2; i <= sqrtNumber; i++ {
+		if number%i == 0 {
+			return false
+		}
+
+	}
+	return true
+
+}
 
 func PrimeX(number int) int {
-	prima := [10]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29}
 
-	return prima[number-1]
+	var x int = -1
+	for i := 0; i < number; i++ {
+		x = x + 1
+		for !checkNumber(x) {
+			// fmt.Println("input", x)
+			x++
+			// fmt.Println("out", x)
+		}
+		// fmt.Println("setelah", x)
+
+	}
+	return x
 }
+
 func main() {
 	fmt.Println(PrimeX(1))  //2
 	fmt.Println(PrimeX(5))  //11
 	fmt.Println(PrimeX(8))  //19
 	fmt.Println(PrimeX(9))  //23
 	fmt.Println(PrimeX(10)) //29
+
 }
